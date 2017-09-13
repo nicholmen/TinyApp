79ab745d154ffaf0572264823f8870198d096f7d
Thomas Nicholson <nicholmen@gmail.com>
2017-09-13 22:30:32 +0000

    making a repo for TINYAPP project

---
+++ express_server.js
@@ -0,0 +1,82 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+//The body-parser library will allow us to access POST request parameters, such as req.body.longURL,
+//which we will store in a variable called urlDatabase
+const randomstring = require("randomstring");
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+  return (randomstring.generate(6))
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+// add a new route handler for "/urls" and use res.render() to pass the URL data to your template.
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  console.log(templateVars)
+  res.render("urls_index", templateVars);
+});
+
+
+// We will use the urls_new.ejs template to render the endpoint, /urls/new.
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//Add a new route in express_server.js which you'll use to render this new template.
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] } ;
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  // generate randomstring
+  let longURL = req.body.longURL;
+  let randomURL = generateRandomString();
+  urlDatabase[randomURL] = longURL;
+  // ^^ add req.body.longURL to urlDatabase with key randomstring
+
+
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${randomURL}`)
+});
+
+// // In your Express server, add the following route to handle shortURL requests:
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+// console.log(randomstring.generate(7))
+
+ // urlDatabase['cats'] = "meow"
+
